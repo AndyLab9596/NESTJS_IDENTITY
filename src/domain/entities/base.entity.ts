c58ce622d325a29aace7d6541a11bb69696b9503
MAC <mac@192.168.2.24>
2025-07-02 21:07:39 +0700

    feat(db) add migration and seeding

---
+++ src/domain/entities/base.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class IdentityEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+}
+
+export abstract class BaseEntity extends IdentityEntity {
+  @CreateDateColumn({ select: false })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ select: false })
+  updatedAt!: Date;
+
+  @Column({ type: 'boolean', default: false, select: false })
+  isDeleted!: boolean;
+}
