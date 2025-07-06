4063a1d8b58fa4b40b56252444d2fbfd8c6810f8
MAC <mac@MacBook-Pro-cua-MAC.local>
2025-07-06 09:51:58 +0700

    (feat) refresh token controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { CreatedResponseDto } from 'src/common/dtos/response/createdResponseDto';
 import { AuthService } from './auth.service';
 import { AuthRequestDto } from './dtos/request/AuthRequestDto';
@@ -24,5 +24,13 @@ export class AuthController {
     @Res({ passthrough: true }) response: Response,
   ) {
     return await this.authService.signIn(authSignInRequestDto, response);
+  }
+
+  @Get('refresh-token')
+  public async refreshToken(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return await this.authService.refreshToken(request, response);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthRequestDto } from './dtos/request/AuthRequestDto';
 import { Repository } from 'typeorm';
 import { User } from 'src/domain/entities';
@@ -9,12 +14,13 @@ import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { JwtService } from '@nestjs/jwt';
 import { v4 as uuidv4 } from 'uuid';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from 'src/config/jwt.config';
 import {
   COOKIE_REFRESH_TOKEN,
   PREFIX_TOKEN_IAT,
+  REQUEST_USER,
 } from './constants/auth.constant';
 
 @Injectable()
@@ -110,6 +116,76 @@ export class AuthService {
       data: {
         userId: user.id,
         accessToken,
+      },
+    };
+  }
+
+  public async refreshToken(request: Request, response: Response) {
+    const refreshToken = request.cookies[COOKIE_REFRESH_TOKEN];
+    if (!refreshToken) {
+      throw new UnauthorizedException();
+    }
+
+    const verifyRefreshToken = await this.jwtService.verifyAsync(refreshToken);
+
+    if (!verifyRefreshToken) {
+      throw new UnauthorizedException();
+    }
+
+    const requestUser = request[REQUEST_USER];
+    if (!requestUser) {
+      throw new UnauthorizedException();
+    }
+
+    const isRefreshTokenInRedis = await this.cacheManager.get(
+      `${PREFIX_TOKEN_IAT}_${requestUser.sub}_${requestUser.jit}`,
+    );
+
+    if (isRefreshTokenInRedis) {
+      response.clearCookie(COOKIE_REFRESH_TOKEN, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: this.MAX_AGE_COOKIE,
+      });
+
+      await this.cacheManager.del(
+        `${PREFIX_TOKEN_IAT}_${requestUser.sub}_${requestUser.jit}`,
+      );
+    }
+
+    const payload = {
+      sub: requestUser.sub,
+      email: requestUser.email,
+      jit: uuidv4(),
+      iat: Math.floor(Date.now() / 1000), // milisecond -> second
+    };
+
+    const newAccessToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.jwtConfiguration.accessTokenExpiresIn!,
+    });
+
+    const newRefreshToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.jwtConfiguration.refreshTokenExpiresIn!,
+    });
+
+    await this.cacheManager.set(
+      `${PREFIX_TOKEN_IAT}_${requestUser.sub}_${payload.jit}`,
+      newRefreshToken,
+    );
+
+    response.cookie(COOKIE_REFRESH_TOKEN, newRefreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: this.MAX_AGE_COOKIE,
+    });
+
+    return {
+      message: 'Sign in successfully',
+      data: {
+        userId: requestUser.sub,
+        accessToken: newAccessToken,
       },
     };
   }
